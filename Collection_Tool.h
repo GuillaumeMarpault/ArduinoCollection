350e59e76bced193bacd3dab60e3c6d8f1b8847c
GuillaumeMarpault <35503359+GuillaumeMarpault@users.noreply.github.com>
2018-01-16 21:55:30 +0100

    Update Collection_Tool.h

--- Collection_Tool.h
+++ Collection_Tool.h
@@ -1,38 +1,4 @@
 
-
-// #############################################################################
-// #
-// # Name		: Collection_Tool.h
-// # Version		: 1.0
-// # Author		: Guillaume Marpault <guillaume.marpault@sfr.fr>
-// # Date		: 16/01/2018
-//
-// # Description: [FR] Méthodes utilisés par les classes conteneur :
-//    - Une implémentation basique des assertions avec une sortie
-//    sur le port série.
-//    - La méthode nextPowerOfTwo utlisé pour le calcul de l'allocation
-//    mémoire de la classe Vector. Cette méthode est extraite du code
-//    source de Qt :
-//    https://code.woboq.org/qt5/qtbase/src/corelib/kernel/qmath.h.html
-//    - Une implémentation basique de l'opérateur 'placement new'.
-//
-// #  This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//	
-// #############################################################################
-
-
-
 #ifndef COLLECTION_TOOL_H
 #define COLLECTION_TOOL_H
 
