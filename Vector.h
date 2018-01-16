c51b556e636cd59c6dfa92b452bf39e96da80f10
GuillaumeMarpault <35503359+GuillaumeMarpault@users.noreply.github.com>
2018-01-16 21:57:10 +0100

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -1,32 +1,4 @@
 
-
-// #############################################################################
-// #
-// # Name		: Vector.h
-// # Version		: 1.0
-// # Author		: Guillaume Marpault <guillaume.marpault@sfr.fr>
-// # Date		: 16/01/2018
-//
-// # Description: [FR] Conteneur indéxé basé sur un array.
-//    Les classes Vector sont partagés implicitement et ont un mécanisme
-//    de copy-on-write. Fourni un temps d'accès constant en lecture.
-//
-// #  This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//	
-// #############################################################################
-
-
 #ifndef VECTOR_H
 #define VECTOR_H
 
